f3572907c6ba3be588e603eb6f6d360672f17c1a
hasanur-dev <59501123+hasanur-dev@users.noreply.github.com>
2024-05-22 17:48:38 +0530

    Update Option.jsx

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -6,15 +6,9 @@ export default function Option({
     checked,
 }) {
     return (
-        // <button
-        //     onClick={() => handleAnswer(option)}
-        //     className={`${correct ? 'bg-green-200' : 'hover:bg-indigo-200/60'} ${checked ? 'pointer-events-none' : ''}  ${wrong ? ' border-red-200 bg-red-200/70 text-red-800 hover:bg-red-200/70' : 'hover:bg-indigo-200/60'} correct cursor-pointer rounded-md border border-indigo-300 bg-indigo-50/50 px-4 py-2.5 font-medium text-gray-800 transition-colors duration-200`}
-        // >
-        //     <p>{option}</p>
-        // </button>
         <button
             onClick={() => handleAnswer(option)}
-           className={`${correct ? 'correct' : ''} ${wrong ? 'wrong' : ''}`}
+            className={`${checked ? 'pointer-events-none' : ''} ${correct ? 'correct' : 'hover:bg-indigo-200/60'} ${wrong ? 'wrong' : 'hover:bg-indigo-200/60'} cursor-pointer rounded-md border border-indigo-300 bg-indigo-50/50 px-4 py-2.5 font-medium text-gray-800 transition-colors duration-200`}
         >
             {option}
         </button>
